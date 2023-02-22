1c1cdea6f8e0c146d5249a3b3f7a78f0829f6a9e
dpetker <dpetker@users.noreply.github.com>
2023-02-22 13:10:24 -0500

    Quick first implementation of /solve endpoint.

--- server/api.ts
+++ server/api.ts
@@ -3,6 +3,7 @@
  */
 
 import express from 'express';
+import { findMatches, Puzzle } from './solver';
 
 console.log('Starting API...');
 
@@ -14,7 +15,27 @@ app.get('/', (req, res) => {
 });
 
 app.get('/solve', (req, res) => {
-  res.send(`Required: ${req.query.req} | Optional: ${req.query.opt}`);
+  const puzz = new Puzzle();
+
+  if (!req.query.req) {
+    res.status(400).send('Missing "req" query string parameter.');
+    return;
+  } else {
+    puzz.requiredChar = req.query.req.toString().toLocaleLowerCase();
+  }
+
+  if (!req.query.opt) {
+    res.status(400).send('Missing "opt" query string parameter.');
+    return;
+  } else {
+    puzz.optionalChars = req.query.opt.toString().toLocaleLowerCase().split('');
+  }
+
+  console.log(`Finding results for puzzle: ${puzz}`);
+  findMatches(puzz).then(matches => {
+    console.log(`Found matches: ${matches}`);
+    res.send(matches);
+  });
 });
 
 app.listen(port, () => {
